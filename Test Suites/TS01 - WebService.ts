87c227a1d29d3db60d39ebf96e45b36d339c3e4c
Md Borhan Uddin <borhan@bs-23.net>
2019-11-18 15:48:13 +0600

    Rest API Services Test

---
+++ Test Suites/TS01 - WebService.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01 - WebService</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>bee1b8e9-fc90-4281-b9ed-cd791a093426</testSuiteGuid>
+   <testCaseLink>
+      <guid>8fcf3fa2-a2f5-4910-a6f9-beb39245dc24</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_ListUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>59c07ba9-a22d-4b17-b351-5c0a1aa02e45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02_HTTPMethod</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
